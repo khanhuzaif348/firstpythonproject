75442f9850c9f6f8757d4ed873dd6ea550e398e4
khanhuzaif348 <103990272+khanhuzaif348@users.noreply.github.com>
2022-04-19 13:34:39 +0530

    Create num.py

---
+++ num.py
@@ -0,0 +1,8 @@
+#Find the Area of Circle
+a = int(input("enter anum"))
+b = 3.14*(a*a) #formulla of Area of circle
+print("your answer is : ",b)
+a = int(input("enter a length"))
+b = int(input("enter a breadth"))
+c = (a*b)
+print("Answer is : ",c)
